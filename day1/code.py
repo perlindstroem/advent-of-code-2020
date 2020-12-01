a137eafe89324156985b15409ca4d767df8bbb52
Per Lindström <perlindstroem@gmail.com>
2020-12-01 15:27:24 +0100

    add part 2

--- day1/code.py
+++ day1/code.py
@@ -14,7 +14,20 @@ def find_product(numbers, desired_sum):
       if n + m == desired_sum: 
         return n*m
 
+'''
+tries to add all numbers to find n, m, o which gives desired_sum
+returns the product of n, m, o
+'''
+def find_product2(numbers, desired_sum):
+  for n in numbers:
+    for m in numbers: 
+      for o in numbers: 
+        if n + m + o == desired_sum: 
+          return n*m*o
+
 data = read_data('data.txt')
 product = find_product(data, 2020)
+product2 = find_product2(data, 2020)
 
 print(f'desired product is {product}')
+print(f'desired product2 is {product2}')
